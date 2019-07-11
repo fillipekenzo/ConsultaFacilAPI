2e375629413b278de7215852bc15ed8645864599
Fabrica <Fabrica@CGR-24019.senacmsedu.local>
2019-07-11 17:33:48 -0400

    api

--- CFApi/Migrations/20190711211417_tet.Designer.cs
+++ CFApi/Migrations/20190711211417_tet.Designer.cs
@@ -3,14 +3,16 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace CFApi.Migrations
 {
     [DbContext(typeof(CFContext))]
-    partial class CFContextModelSnapshot : ModelSnapshot
+    [Migration("20190711211417_tet")]
+    partial class tet
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ CFApi/Migrations/20190711211417_tet.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace CFApi.Migrations
+{
+    public partial class tet : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
